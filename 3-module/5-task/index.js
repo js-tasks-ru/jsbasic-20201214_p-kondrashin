60fe4abe29efc2eca91d67c090bc24bf778f2457
Petr.Ko <p.kondrashin@gmail.com>
2020-12-24 21:57:58 +0300

    3-module/4-5-tasks DONE

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,15 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
+ 
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ").join(',').split(', ').join().split(',')
+      .filter(elem => Number(elem))
+      .sort((a, b) => a - b);
+
+  let min = Number(arr.shift()),
+      max = Number(arr.pop());
+
+  return {min, max} 
 }
