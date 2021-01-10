6a9f30034854fa2d28c69e3490fa8378ef34b536
Petr.Ko <p.kondrashin@gmail.com>
2021-01-10 21:28:26 +0300

    4-module/2-task переписал на одном forEach

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,16 +2,9 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
+
 function makeDiagonalRed(table) {
-    let i = 0; 
-
-    [...table.rows].forEach (elem => {
-        [...elem.cells].forEach (item => {
-            if (item.cellIndex == i) {
-                item.style.background ='red';
-            }
-        });
-
-        i++
+    [...table.rows].forEach ((elem, index) => {
+      elem.cells[index].style.background = 'red'
     });
 }
