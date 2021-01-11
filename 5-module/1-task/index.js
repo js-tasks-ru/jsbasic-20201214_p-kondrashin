8763d2893880a4fb6432336bf6d5726b1b39d58d
Petr.Ko <p.kondrashin@gmail.com>
2021-01-11 16:37:19 +0300

    5-module/1-task убрал функцию hide

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,7 +1,4 @@
 function hideSelf() {
   let button = document.querySelector('.hide-self-button');
-  
-  const hide = () => {button.hidden = true}
-
-  button.addEventListener('click', hide);
+  button.addEventListener('click', () => button.hidden = true);
 }
