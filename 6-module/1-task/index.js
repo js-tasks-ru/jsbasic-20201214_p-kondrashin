0920fb727c4ed090e30161179e90bac113236c2d
Petr.Ko <p.kondrashin@gmail.com>
2021-01-13 21:38:44 +0300

    Добавил свойство, но все равно ничего не работает((

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -31,19 +31,22 @@
 export default class UserTable {
   constructor(rows) {
       this.rows = rows;
+      this.elem = "";
   }
+  
   render() {
+    this.elem = template(cells);
     let cells = '';
-
+    
     this.rows.forEach(elem => makeTempateCells(elem));
 
     function makeTempateCells({name, age, salary, city} = {elem}){
         cells += `<tr><td>${name}</td><td>${age}</td><td>${salary}</td><td>${city}</td><td><button>X</button></td></tr>`
-    }
+    } 
 
-    function makeTemplateTable(string) {
-        return `<table><thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody>${string}</tbody></table>`
+    const template = function makeTemplateTable(string) {
+      return `<table><thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody>${string}</tbody></table>`
     }
-    console.log(makeTemplateTable(cells));
   }
+
 }
