9562e9de76f5a80a6a15c99dff850fc689217d50
Petr.Ko <p.kondrashin@gmail.com>
2021-01-19 21:56:17 +0300

    7-module/1-task промежуточное решение

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,42 @@
 import createElement from '../../assets/lib/create-element.js';
 
+function makeLayout(data) {
+  let layoutItems = '';
+
+  function makeLayoutItems({name, id} = {elem}) {
+    layoutItems+=`<a href="#" class="ribbon__item" data-id="${id}">${name}</a>`
+  }
+
+  data.forEach(elem => makeLayoutItems(elem));
+
+  function makeLayoutElement(items) {
+    return ` <div class="ribbon">
+    <button class="ribbon__arrow ribbon__arrow_left">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+    <nav class="ribbon__inner">
+     ${items}
+    </nav>
+    <button class="ribbon__arrow ribbon__arrow_right  ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+  </div>`
+  }
+
+  return makeLayoutElement(layoutItems)
+}
+// let menu = document.querySelector('.ribbon__inner').scrollBy(350, 0)
+
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = createElement(makeLayout(this.categories));
+    this.container = this.elem.querySelector('.ribbon__inner');
+    this.scroll()
+  }
+
+  scroll() {
+    console.log(this.container);
+    this.container.scrollBy(700,0); 
   }
 }
