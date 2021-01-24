b9a05cb49b3e4235a21ba296b490802b7c4b1ee5
Petr.Ko <p.kondrashin@gmail.com>
2021-01-25 00:47:59 +0300

    7-module/3-task DONE

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,75 @@
+function createElement(spans) {
+  let div = document.createElement('div')
+  div.innerHTML = `
+  <!--Корневой элемент слайдера-->
+    <!--Ползунок слайдера с активным значением-->
+    <div class="slider__thumb" style="left: 0%;">
+      <span class="slider__value">3</span>
+    </div>
+    <!--Заполненная часть слайдера-->
+    <div class="slider__progress" style="width: 0%;"></div>
+    <!--Шаги слайдера-->
+    <div class="slider__steps">
+      ${spans}
+    </div>`
+  div.classList.add('slider');
+  return div
+}
+
 export default class StepSlider {
-  constructor({ steps, value = 0 }) {
+  constructor({steps, value = 0}) {
+    this.steps = steps;
+    this.value = value;
+    this.elem = createElement(this.createsteps());
+    this.changevalueslider(this.value);
+    this.initlistener();
   }
+
+  createsteps() {
+    let steps = '';
+    for(let i = 0; i < this.steps; i++) {
+      steps+=`<span></span>`
+    }
+    return steps
+  }
+  
+  changevalueslider(value,valuePercents) {
+    let 
+      sliderValue = this.elem.querySelector('.slider__value'),
+      sliderSteps = this.elem.querySelector('.slider__steps'),
+      thumb = this.elem.querySelector('.slider__thumb'),
+      progress = this.elem.querySelector('.slider__progress');
+
+    sliderValue.textContent = value;
+     
+    for(let elem of sliderSteps.children) {
+      elem.classList.remove('slider__step-active');
+    }
+
+    sliderSteps.children[value].classList.add('slider__step-active');  
+
+    thumb.style.left = `${valuePercents}%`;
+
+    progress.style.width = `${valuePercents}%`;
+  }
+
+  initlistener() {
+    this.elem.addEventListener('click', (event) => {
+      let 
+        left = event.clientX - this.elem.getBoundingClientRect().left,
+        leftRelative = left / this.elem.offsetWidth,
+        segments = this.steps - 1,
+        approximateValue = leftRelative * segments,
+        value = Math.round(approximateValue),
+        valuePercents = value / segments * 100;
+      this.changevalueslider(value, valuePercents);
+      this.addevent(value);
+    });
+  }
+
+  addevent(value) {
+    let event = new CustomEvent('slider-change', {detail: value, bubbles: true});
+    this.elem.dispatchEvent(event);
+  }
+
 }
