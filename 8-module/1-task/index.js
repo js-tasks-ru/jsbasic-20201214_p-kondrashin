67222cdb37a222ca4569b0a04b6469661fcf6ab2
Petr.Ko <p.kondrashin@gmail.com>
2021-01-28 19:38:18 +0300

    8-module/1-2 task DONE

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,37 +39,44 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    let initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
-    console.log(initialTopCoord);
-    if (window.pageYOffset > initialTopCoord) {
+    if (!this.initialTopCoord) {
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    if (window.pageYOffset > this.initialTopCoord) {
       let leftIndent = Math.min(
         document.querySelector('.container').getBoundingClientRect().right + 20,
         document.documentElement.clientWidth - this.elem.offsetWidth - 10
       ) + 'px'
-      
-      Object.assign(this.elem.style, {
-        position: 'fixed',
-        top: '50px',
-        zIndex: 1e3,
-        right: '10px',
-        left: leftIndent
-      });
-    } else {
-      Object.assign(this.elem.style, {
-        position: '',
-        top: '',
-        left: '',
-        zIndex: ''
-      });
-
-      if (document.documentElement.clientWidth <= 767) {
-        Object.assign(this.elem.style, {
-          position: '',
-          top: '',
-          left: '',
-          zIndex: ''
-        });
-      }
+       
+     this.fixedPosition(leftIndent);
+    } 
+    else {
+      this.resetPosition();
     }
+    
+    if (document.documentElement.clientWidth <= 767) {
+      this.resetPosition();
+    }
+  }
+
+  fixedPosition(leftIndent) {
+    console.log(this.elem)
+    Object.assign(this.elem.style, {
+      position: 'fixed',
+      top: '50px',
+      zIndex: 1e3,
+      right: '10px',
+      left: leftIndent
+    });
+  }
+  resetPosition() {
+    Object.assign(this.elem.style, {
+      position: '',
+      top: '',
+      left: '',
+      zIndex: ''
+    });
+
   }
 }
